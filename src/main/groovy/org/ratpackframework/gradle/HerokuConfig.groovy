f305bec5f34a1fe88671a92d45d85af87fbedc72
Marco Vermeulen <vermeulen.mp@gmail.com>
2013-08-15 19:57:05 +0100

    Removed hardcoded values from heroku config.

--- src/main/groovy/org/ratpackframework/gradle/HerokuConfig.groovy
+++ src/main/groovy/org/ratpackframework/gradle/HerokuConfig.groovy
@@ -1,7 +1,7 @@
 package org.ratpackframework.gradle
 
 class HerokuConfig {
-    String appName = 'fast-everglades-6675'
-    String apiKey = 'invalid'
-    String buildpack = "http://dl.bintray.com/vermeulen-mp/heroku-buildpack-ratpack/heroku-buildpack-ratpack.zip"
+    String appName
+    String apiKey
+    String buildpack
 }
